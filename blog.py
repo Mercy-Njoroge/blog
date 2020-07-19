5a89603621cb36e4fbb8c0627916292aa833ba33
Mercy <aprilkasha42@gmail.com>
2020-07-19 22:58:07 +0300

    create blog.py file add posts method,create routes to home,about,register and login templates,generate a secret key in register and login method add methods function with GET and POST

---
+++ blog.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c0b19dcf6b8df87d54a1ea13ab216be6'
+
+posts = [
+    {
+        'author': 'Mercy Njoroge',
+        'title':'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 18,2020'
+    },
+    {
+        'author': 'Phylis Njeri',
+        'title':'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 19,2020'
+    }
+
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+   form = RegistrationForm()
+   if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+   return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+   return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
