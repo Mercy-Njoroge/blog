112bf1ec92bdffcc98f7173169c6973d96d65886
Mercy <aprilkasha42@gmail.com>
2020-07-22 03:59:05 +0300

    add start.sh file,fix home.html and layout.html file

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='c0b19dcf6b8df87d54a1ea13ab216be6'
+export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+export MAIL_USERNAME='aprilkasha42@gmail.com'
+export MAIL_PASSWORD='phylmary2'
+
+python run.py server
